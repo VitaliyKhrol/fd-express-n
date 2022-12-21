42b360bfc52d8fbd3757749cef4f513fe9123e32
VitaliyKhrol <vitaliyx@gmail.com>
2022-12-21 22:50:59 +0200

    init

---
+++ controllers/User.controller.js
@@ -0,0 +1,71 @@
+const User = require('../models/User');
+
+
+module.exports.createUser = async (req, res, next) => {
+        try {
+                const {body} = req;
+                const createdUser = await new User(body);
+                const result = await createdUser.save();
+                 res.status(201).send(result);
+        } catch(error) {
+                next(error);
+        }
+
+}
+
+module.exports.getAllUsers = async (req, res, next) => {
+        try {
+                const users = await User.findAll();
+                res.status(200).send(users);
+        }catch(error) {
+                next(error)
+        }
+}
+
+module.exports.getOneUser = async (req, res, next) => {
+        try {
+                const {params: {userId}, userInstance} = req;
+                res.send(userInstance);
+        } catch (error) {
+                next(error)
+        }
+
+}
+
+module.exports.updateUser = async (req, res, next) => {
+        try {
+                const {params: {userId}, body, userInstance} = req;
+                const updated = await userInstance.update(body);
+                res.status(200).send(updated);
+        } catch(error) {
+                next(error);
+        }
+}
+
+module.exports.deleteUser = async (req, res, next) => {
+        try {
+                const {params: {userId}, userInstance} = req;
+               const result = await userInstance.delete();
+                res.status(200).send('User deleted');
+        } catch(error) {
+              next(error);
+        }
+
+}
+
+module.exports.loginUser = async(req, res, next) => {
+        try {
+                const{params: {userId}, body, userInstance} = req;
+               if(userInstance.email === body.email){
+                        if(userInstance.password === body.password) {
+                                res.status(202).send('Authorized succesfully')
+                        } else {
+                                res.status(401).send('Password incorrect');
+                        }
+               } else {
+                res.status(403).send('Incorrect data');
+               }
+        } catch(error) {
+                next(error);
+        }
+}
